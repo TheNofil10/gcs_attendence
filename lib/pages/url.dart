fe17582df4cd38e499aa837d5fb1c9c5b011e272
Nofil Iqbal <nofil.iqbal10@gmail.com>
2024-12-04 10:40:49 +0500

    ios physical testing done

--- lib/pages/url.dart
+++ lib/pages/url.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 final String crmBaseUrl = Platform.isIOS
-    // ? 'http://192.168.10.60:8000/'  // For iOS physical
-    ? 'http://localhost:8000/'  // For iOS simulator
+    ? 'http://192.168.10.60:8000/'  // For iOS physical
+    // ? 'http://localhost:8000/'  // For iOS simulator
 
     : 'http://10.0.2.2:8000/';  // For Android emulator
 
 final String facialBaseUrl = Platform.isIOS
-    // ? 'http://localhost:8001/'  // For iOS physical
-    ? 'http://192.168.10.60:8001/'  // For iOS simulator
+    ? 'http://192.168.10.60:8001/'  // For iOS physical
+    // ? 'http://localhost:8001/'  // For iOS simulator
+
 
     : 'http://10.0.2.2:8001/';  // For Android emulator
 
